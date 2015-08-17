8092bab2bcc59635ef71b863273b97c5b0ae4eb8
HSLee <peanutstars.lee@gmail.com>
2015-08-17 16:31:32 +0900

    Add ubuntu-14.04-build.sh script for setting a build envrironmnet.

---
+++ init/ubuntu-14.04-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install vim git ssh subversion p7zip-full exuberant-ctags ckermit wget curl
+sudo apt-get install squashfs-tools libtool automake ccache
+sudo apt-get install build-essential
+sudo apt-get install gcc-multilib
+sudo apt-get install flex bison gperf zip 
+sudo apt-get install libc6-dev libncurses5-dev:i386 libreadline6-dev:i386 g++-multilib zlib1g-dev:i386
