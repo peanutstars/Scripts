99a6db21382d040ac19ed73719dff4fe2dd1b6ca
HSLee <peanutstars.lee@gmail.com>
2015-08-19 16:41:28 +0900

    update environment variable in bash.

--- init/ubuntu-14.04-build.sh
+++ init/ubuntu-14.04-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install vim git gitk ssh subversion 
-sudo apt-get install p7zip-full exuberant-ctags ckermit wget curl
-sudo apt-get install flex bison gperf zip 
+sudo apt-get install vim git gitk ssh subversion gawk aptitude
+sudo apt-get install p7zip-full exuberant-ctags ckermit wget curl meld
+sudo apt-get install flex bison gperf zip ncurses-dev libssl-dev openssl 
 sudo apt-get install squashfs-tools libtool automake ccache
 sudo apt-get install build-essential
 sudo apt-get install gcc-multilib
 sudo apt-get install g++-multilib
-sudo apt-get install libc6-dev libncurses5-dev:i386 libreadline6-dev:i386 zlib1g-dev:i386
+sudo apt-get install libc6-dev zlib1g-dev
+sudo apt-get install libncurses5-dev:i386 libreadline6-dev:i386 zlib1g-dev:i386
