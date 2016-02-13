0a25af10cfc744d01538f5d255c48d202cd7a576
peanutstars <peanutstars.lee@gmail.com>
2016-02-14 05:44:03 +0900

    Update ubuntu-14.04-build.sh

--- init/ubuntu-14.04-build.sh
+++ init/ubuntu-14.04-build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+USER=`cat /etc/passwd | grep 1000 | awk -F: '{ print $1 }'`
+
 sudo apt-get update
 sudo apt-get install vim git gitk ssh subversion gawk aptitude
 sudo apt-get install p7zip-full exuberant-ctags ckermit wget curl meld
 sudo apt-get install flex bison gperf zip ncurses-dev libssl-dev openssl 
-sudo apt-get install minissdpd
+#sudo apt-get install minissdpd
 sudo apt-get install squashfs-tools
 sudo apt-get install libtool automake ccache
 sudo apt-get install build-essential
 sudo apt-get install gcc-multilib
 sudo apt-get install g++-multilib
 sudo apt-get install libc6-dev zlib1g-dev
-sudo apt-get install libncurses5-dev:i386 libreadline6-dev:i386 zlib1g-dev:i386
+#sudo apt-get install libncurses5-dev:i386 libreadline6-dev:i386 zlib1g-dev:i386
 sudo apt-get install manpages-posix manpages-posix-dev
-sudo usermod -a -G uucp,dialout `whoami`
+sudo usermod -a -G uucp,dialout $USER
